289e9a85dc0326914d20f3080166d9b786b90af4
James Michael <jomichael1994@gmail.com>
2025-06-16 17:29:49 +0100

    wip user selection

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -2,7 +2,10 @@ import Users from './Users';
 
 const Auth = () => {
 	return (
-		<div className="h-full bg-yellow-200 flex justify-center items-center">
+		<div className="h-full bg-slate-900 flex justify-center items-center">
+			<span className="fixed top-4 left-8 font-hubot font-medium text-neutral-200 text-2xl">
+				Vault
+			</span>
 			<Users />
 		</div>
 	);
--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,13 +1,23 @@
+import User from './User';
+
+import { FaPlus } from 'react-icons/fa6';
+
+const users = [{ name: 'James' }, { name: 'Test User' }];
+
 const Users = () => {
-	const users = ['1', '2'];
 	return (
-		<div className="flex flex-wrap gap-8">
-			{users?.map((user, i) => (
-				<div key={i} className="w-56 h-64 bg-yellow-400">
-					<span>{user}</span>
-				</div>
+		<div className="flex flex-wrap justify-center items-center gap-8">
+			{users?.map(({ name }, i) => (
+				<User key={i} name={name} />
 			))}
-			<div className="w-56 h-64 bg-yellow-400">Add</div>
+			<div className="group transition-all duration-200 hover:cursor-pointer p-4 flex flex-col gap-4 md:gap-5 justify-center items-center text-white w-40 h-48 md:w-56 md:h-64 rounded-xl">
+				<div className="transition-all duration-150 text-6xl md:text-8xl group-hover:text-slate-200 text-slate-500 flex justify-center items-center h-24 md:h-36">
+					<FaPlus />
+				</div>
+				{/* <span className="transition-all duration-150 leading-none font-hubot text-md md:text-lg font-medium text-slate-500 group-hover:text-slate-200 ">
+					Add user
+				</span> */}
+			</div>
 		</div>
 	);
 };
