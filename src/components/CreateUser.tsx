a5a895816cbe1a358a1e2cad5999bfe00eddf4e5
James Michael <jomichael1994@gmail.com>
2025-06-16 18:38:39 +0100

    add initial user creation form

---
+++ src/components/CreateUser.tsx
@@ -0,0 +1,22 @@
+import { LuUserRoundPlus } from 'react-icons/lu';
+import Input from './shared/Input';
+import Button from './shared/Button';
+
+const CreateUser = () => {
+	return (
+		<div className="h-full w-full flex flex-col gap-4 items-center mt-60 text-slate-200">
+			<LuUserRoundPlus className="text-7xl" />
+			<span className="font-hubot text-xl font-bold leading-none">
+				Create user
+			</span>
+			<div className="flex flex-col gap-4 mt-2 p-4 md:p-8 w-full md:w-[27rem] bg-slate-800 outline-1 outline-slate-400 rounded-xl">
+				<Input placeholder="Username" />
+				<Input placeholder="Password" />
+				<Input placeholder="Confirm password" />
+				<Button text="Create" />
+			</div>
+		</div>
+	);
+};
+
+export default CreateUser;
