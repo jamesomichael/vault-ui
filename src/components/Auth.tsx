3b69ef82a274fc654027386f74771cf5f8896c26
James Michael <jomichael1994@gmail.com>
2025-06-16 18:39:06 +0100

    test form integration

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
+
 import Users from './Users';
+import LogInForm from './LogInForm';
+import CreateUser from './CreateUser';
 
 const Auth = () => {
+	const [showLogInForm, setShowLogInForm] = useState(false);
+	const [showCreationForm, setShowCreationForm] = useState(false);
+
 	return (
-		<div className="h-full bg-slate-900 flex justify-center items-center">
+		<div className="h-full px-4 bg-slate-900 flex justify-center items-center">
 			<span className="fixed top-4 left-8 font-hubot font-medium text-neutral-200 text-2xl">
 				Vault
 			</span>
-			<Users />
+			{showLogInForm ? (
+				<LogInForm />
+			) : showCreationForm ? (
+				<CreateUser />
+			) : (
+				<Users />
+			)}
 		</div>
 	);
 };
