a239fc6ef1e70e147322fe2fc1737e23083bfde0
James Michael <jomichael1994@gmail.com>
2025-06-17 16:10:01 +0100

    add FieldGroup types

--- src/components/vault/active-item/FieldGroup.tsx
+++ src/components/vault/active-item/FieldGroup.tsx
@@ -1,4 +1,11 @@
-const FieldGroup = ({ className, children }) => {
+import React from 'react';
+
+interface FieldGroupProps {
+	className?: string;
+	children: React.ReactNode;
+}
+
+const FieldGroup = ({ className, children }: FieldGroupProps) => {
 	return (
 		<div
 			className={`w-full bg-slate-700 rounded flex flex-col divide-y-[1px] divide-slate-500 ${className}`}
