c18fe9441b433a220fbb3741304745ea2a77c1a4
James Michael <jomichael1994@gmail.com>
2025-06-18 15:10:43 +0100

    add EmptyList component

--- src/components/vault/item-panel/list/VaultList.tsx
+++ src/components/vault/item-panel/list/VaultList.tsx
@@ -3,14 +3,17 @@ import { useSelector } from 'react-redux';
 import { getFilteredItems } from '../../../../redux/vaultSlice';
 
 import VaultListItem from './VaultListItem';
+import EmptyList from './EmptyList';
 
 const VaultList = () => {
 	const items = useSelector(getFilteredItems);
 	return (
 		<div className="h-full bg-slate-800 flex flex-col overflow-y-scroll">
-			{items.map((item, i) => (
-				<VaultListItem key={i} item={item} />
-			))}
+			{items.length > 0 ? (
+				items.map((item, i) => <VaultListItem key={i} item={item} />)
+			) : (
+				<EmptyList />
+			)}
 		</div>
 	);
 };
