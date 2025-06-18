cca31b05cb33f3693137fab5f78eec1c672753b7
James Michael <jomichael1994@gmail.com>
2025-06-18 21:03:33 +0100

    add EditVaultItem component

--- src/components/vault/item-window/edit/EditVaultItem.tsx
+++ src/components/vault/item-window/edit/EditVaultItem.tsx
@@ -1,5 +1,92 @@
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
+
+import FieldGroup from '../../../shared/FieldGroup';
+import Field from '../../../shared/Field';
+
+import type { RootState } from '../../../../redux/store';
+
 const EditVaultItem = () => {
-	return <div className="text-white">EditVaultItem</div>;
+	const activeItem = useSelector(
+		(state: RootState) => state.vault.activeItem
+	);
+
+	if (!activeItem) {
+		return null;
+	}
+
+	const [name, setName] = useState(activeItem.name || '');
+	const [username, setUsername] = useState(activeItem.username || '');
+	const [password, setPassword] = useState(activeItem.password || '');
+	const [uri, setUri] = useState(activeItem.uri || '');
+	const [isFavourite, setIsFavourite] = useState(
+		activeItem.isFavourite || false
+	);
+
+	return (
+		<div className="h-full grid grid-rows-[1fr_3.5rem]">
+			<div className="w-full h-full max-w-screen-md mx-auto flex flex-col gap-3 p-4">
+				<span className="px-2 text-slate-300 uppercase text-sm font-hubot">
+					Add Item
+				</span>
+				<div className="flex flex-col gap-8">
+					<FieldGroup>
+						<Field
+							type="dropdown"
+							dropdownOptions={[{ id: 'login', label: 'Login' }]}
+							isEditable={false}
+							label="Type"
+						/>
+						<Field
+							isEditable={true}
+							onChange={(e) => setName(e.target.value)}
+							label="Name"
+							value={name}
+						/>
+						<Field
+							isEditable={true}
+							onChange={(e) => {
+								setUsername(e.target.value);
+							}}
+							value={username}
+							label="Username"
+						/>
+						<Field
+							isEditable={true}
+							onChange={(e) => setPassword(e.target.value)}
+							value={password}
+							label="Password"
+							isPassword={true}
+						/>
+					</FieldGroup>
+					<FieldGroup>
+						<Field
+							isEditable={true}
+							label="URI"
+							onChange={(e) => setUri(e.target.value)}
+							value={uri}
+							isUri={true}
+						/>
+					</FieldGroup>
+					<FieldGroup>
+						<Field
+							isEditable={true}
+							label="Folder"
+							type="dropdown"
+						/>
+						<Field
+							isEditable={true}
+							label="Favourite"
+							type="checkbox"
+							value={isFavourite}
+							onChange={() => setIsFavourite((prev) => !prev)}
+						/>
+					</FieldGroup>
+				</div>
+			</div>
+			{/* <ViewActions /> */}
+		</div>
+	);
 };
 
 export default EditVaultItem;
