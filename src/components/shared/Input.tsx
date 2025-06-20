6ef248799afcacf5206f8e7657781ed3b4726221
James Michael <jomichael1994@gmail.com>
2025-06-20 20:07:15 +0100

    expand Input component

--- src/components/shared/Input.tsx
+++ src/components/shared/Input.tsx
@@ -1,13 +1,27 @@
 interface InputProps {
 	placeholder?: string;
+	value?: string | null;
+	onChange?: () => void;
 }
 
-const Input = ({ placeholder }: InputProps) => {
+const Input = ({
+	placeholder,
+	type = 'text',
+	value,
+	onChange,
+	disabled = false,
+}: InputProps) => {
+	const isPassword = type === 'password';
 	return (
 		<input
-			type="text"
+			type={type}
+			disabled={disabled}
+			value={value}
+			onChange={onChange}
 			placeholder={placeholder}
-			className="w-full font-hubot rounded-lg outline-1 outline-slate-400 placeholder-slate-400 focus:outline-2 focus:outline-blue-400 hover:outline-blue-400 text-xs sm:text-sm h-10 px-3 py-2"
+			className={`w-full rounded-lg outline-1 outline-slate-400 placeholder-slate-400 focus:outline-2 focus:outline-blue-400 hover:outline-blue-400 text-xs sm:text-sm h-10 px-3 py-2 ${
+				isPassword ? '' : 'font-hubot'
+			}`}
 		/>
 	);
 };
