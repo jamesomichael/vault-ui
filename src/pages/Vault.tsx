8b06f871837f16e4852b9f06bc80e540f69d44e5
James Michael <jomichael1994@gmail.com>
2025-06-21 21:22:30 +0100

    integrate fetch logic

--- src/pages/Vault.tsx
+++ src/pages/Vault.tsx
@@ -1,12 +1,25 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
 import ItemWindow from '../components/vault/item-window/ItemWindow';
 import Header from '../components/vault/header/Header';
 import ItemPanel from '../components/vault/item-panel/ItemPanel';
 import Sidebar from '../components/vault/sidebar/Sidebar';
 
+import { fetchItems } from '../redux/vaultSlice';
+
 import { useEncryptionKey } from '../hooks/useEncryptionKey';
 
 const Vault = () => {
-	useEncryptionKey();
+	const dispatch = useDispatch();
+	const { encryptionKey } = useEncryptionKey();
+
+	useEffect(() => {
+		if (encryptionKey) {
+			dispatch(fetchItems({ key: encryptionKey }));
+		}
+	}, [encryptionKey, dispatch]);
+
 	return (
 		<div className="h-full grid grid-rows-[3.2rem_1fr]">
 			<Header />
