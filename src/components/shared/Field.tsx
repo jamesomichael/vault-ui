ff8adeae124e1a0c898ae64fc47423fb3bd87558
James Michael <jomichael1994@gmail.com>
2025-06-22 22:11:51 +0100

    add password generator

--- src/components/shared/Field.tsx
+++ src/components/shared/Field.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 
+import PasswordGenerator from './PasswordGenerator';
+
+import { LuRefreshCcwDot } from 'react-icons/lu';
 import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa';
 import { FaRegCopy } from 'react-icons/fa6';
 import { FiExternalLink } from 'react-icons/fi';
@@ -32,99 +35,117 @@ const Field = ({
 	canCopy = false,
 }: Props) => {
 	const [isMasked, setIsMasked] = useState(shouldMask);
+	const [showModal, setShowModal] = useState(false);
 
 	const toggleIsMasked = () => setIsMasked((prev) => !prev);
 
 	const copyToClipboard = () => navigator.clipboard.writeText(value);
 
+	const openGenerator = () => {
+		setShowModal(true);
+	};
+
 	return (
-		<div>
-			<div className="group rounded hover:bg-slate-600 focus-within:bg-slate-600 transition-colors duration-200 px-4 py-3 flex items-center justify-between">
-				<div className="w-full flex flex-col justify-center gap-2">
-					{type === 'checkbox' ? (
-						<div className="h-6 flex justify-between items-center">
-							<span className="font-hubot leading-none text-sm text-slate-100">
-								{label}
-							</span>
-							<input
-								type="checkbox"
-								disabled={!isEditable}
-								onChange={onChange}
-								checked={!!value}
-							/>
-						</div>
-					) : (
-						<>
-							<span className="font-hubot leading-none text-xs text-slate-300">
-								{label}
-							</span>
-							{type === 'dropdown' ? (
-								<select
-									disabled={!isEditable}
-									onChange={onChange}
-									value={dropdownOptions[0]?.label}
-									className="font-hubot w-full h-6 leading-none text-sm text-slate-100 outline-none rounded bg-transparent"
-								>
-									{dropdownOptions.map((opt) => (
-										<option
-											key={opt.id}
-											value={opt.label}
-											disabled={!isEditable}
-										>
-											{opt.label}
-										</option>
-									))}
-								</select>
-							) : (
+		<>
+			<div>
+				<div className="group rounded hover:bg-slate-600 focus-within:bg-slate-600 transition-colors duration-200 px-4 py-3 flex items-center justify-between">
+					<div className="w-full flex flex-col justify-center gap-2">
+						{type === 'checkbox' ? (
+							<div className="h-6 flex justify-between items-center">
+								<span className="font-hubot leading-none text-sm text-slate-100">
+									{label}
+								</span>
 								<input
+									type="checkbox"
 									disabled={!isEditable}
 									onChange={onChange}
-									value={
-										isMasked
-											? '•'.repeat(value.length)
-											: value
-									}
-									className={`${
-										isMasked ? '' : 'font-hubot'
-									} w-full h-6 leading-none text-sm text-slate-100 outline-none bg-transparent`}
+									checked={!!value}
 								/>
-							)}
-						</>
-					)}
-				</div>
-				<div className="flex items-center text-xl gap-5 text-slate-300">
-					{(isPassword || shouldMask) && (
-						<span
-							onClick={toggleIsMasked}
-							className="hover:cursor-pointer hover:text-slate-100"
-						>
-							{isMasked ? (
-								<FaRegEye title="Show" />
-							) : (
-								<FaRegEyeSlash title="Hide" />
-							)}
-						</span>
-					)}
-					{isUri && !isEditable && (
-						<a
-							href={value}
-							target="_blank"
-							rel="noopener noreferrer"
-							className="hover:cursor-pointer hover:text-slate-100"
-						>
-							<FiExternalLink title="Launch" />
-						</a>
-					)}
-					{canCopy && (
-						<FaRegCopy
-							title={`Copy ${label.toLowerCase()}`}
-							onClick={copyToClipboard}
-							className="hover:cursor-pointer hover:text-slate-100"
-						/>
-					)}
+							</div>
+						) : (
+							<>
+								<span className="font-hubot leading-none text-xs text-slate-300">
+									{label}
+								</span>
+								{type === 'dropdown' ? (
+									<select
+										disabled={!isEditable}
+										onChange={onChange}
+										value={dropdownOptions[0]?.label}
+										className="font-hubot w-full h-6 leading-none text-sm text-slate-100 outline-none rounded bg-transparent"
+									>
+										{dropdownOptions.map((opt) => (
+											<option
+												key={opt.id}
+												value={opt.label}
+												disabled={!isEditable}
+											>
+												{opt.label}
+											</option>
+										))}
+									</select>
+								) : (
+									<input
+										disabled={!isEditable}
+										onChange={onChange}
+										value={
+											isMasked
+												? '•'.repeat(value.length)
+												: value
+										}
+										className={`${
+											isMasked ? '' : 'font-hubot'
+										} w-full h-6 leading-none text-sm text-slate-100 outline-none bg-transparent`}
+									/>
+								)}
+							</>
+						)}
+					</div>
+					<div className="flex items-center text-xl gap-5 text-slate-300">
+						{(isPassword || shouldMask) && (
+							<span
+								onClick={toggleIsMasked}
+								className="hover:cursor-pointer hover:text-slate-100"
+							>
+								{isMasked ? (
+									<FaRegEye title="Show" />
+								) : (
+									<FaRegEyeSlash title="Hide" />
+								)}
+							</span>
+						)}
+						{isPassword && (
+							<span
+								onClick={openGenerator}
+								className="hover:cursor-pointer hover:text-slate-100"
+							>
+								<LuRefreshCcwDot title="Generate password" />
+							</span>
+						)}
+						{isUri && !isEditable && (
+							<a
+								href={value}
+								target="_blank"
+								rel="noopener noreferrer"
+								className="hover:cursor-pointer hover:text-slate-100"
+							>
+								<FiExternalLink title="Launch" />
+							</a>
+						)}
+						{canCopy && (
+							<FaRegCopy
+								title={`Copy ${label.toLowerCase()}`}
+								onClick={copyToClipboard}
+								className="hover:cursor-pointer hover:text-slate-100"
+							/>
+						)}
+					</div>
 				</div>
 			</div>
-		</div>
+			{showModal && (
+				<PasswordGenerator onClose={() => setShowModal(false)} />
+			)}
+		</>
 	);
 };
 
---
+++ src/components/shared/PasswordGenerator.tsx
@@ -0,0 +1,172 @@
+import { useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
+import generator from 'generate-password-browser';
+
+import Modal from './Modal';
+
+import { LuRefreshCcwDot } from 'react-icons/lu';
+import { FaRegCopy } from 'react-icons/fa6';
+
+interface Props {
+	onClose: () => void;
+}
+
+const PasswordGenerator = ({ onClose }: Props) => {
+	const [password, setPassword] = useState<string>('');
+	const [length, setLength] = useState(14);
+	const [uppercase, setUppercase] = useState(true);
+	const [lowercase, setLowercase] = useState(true);
+	const [numbers, setNumbers] = useState(true);
+	const [symbols, setSymbols] = useState(true);
+
+	const copyToClipboard = () => navigator.clipboard.writeText(password);
+
+	const generatePassword = () => {
+		const password = generator.generate({
+			length,
+			numbers,
+			symbols,
+			lowercase,
+			uppercase,
+		});
+		setPassword(password);
+	};
+
+	useEffect(() => {
+		generatePassword();
+	}, [length, uppercase, lowercase, numbers, symbols]);
+
+	useEffect(() => {
+		if (!uppercase && !lowercase && !numbers && !symbols) {
+			setUppercase(true);
+			setLowercase(true);
+		}
+	}, [uppercase, lowercase, numbers, symbols]);
+
+	const handleLengthChange = (
+		e: React.ChangeEvent<HTMLInputElement>,
+		isBlur = false
+	) => {
+		const newLength = Number(e.target.value);
+		if (isBlur && newLength < 5) {
+			setLength(5);
+		} else if (isBlur && newLength > 128) {
+			setLength(128);
+		} else {
+			setLength(newLength);
+		}
+		generatePassword();
+	};
+
+	return createPortal(
+		<Modal heading="Generator" onClose={onClose}>
+			<div className="select-none flex flex-col gap-5">
+				<div className="flex justify-between items-center w-full bg-slate-800 h-14 rounded-md px-3">
+					<input
+						type="text"
+						disabled={true}
+						placeholder={password}
+						className="select-text font-hubot placeholder-white text-sm font-semibold antialiased"
+					/>
+					<div className="flex items-center gap-1">
+						<div
+							title="Generate password"
+							onClick={() => generatePassword()}
+							className="hover:cursor-pointer rounded-md hover:outline-1 hover:outline-blue-400 transition-all duration-150 p-2"
+						>
+							<LuRefreshCcwDot className="text-slate-100 text-lg" />
+						</div>
+						<div
+							title="Copy password"
+							onClick={copyToClipboard}
+							className="hover:cursor-pointer rounded-md hover:outline-1 hover:outline-blue-400 transition-all duration-150 p-2"
+						>
+							<FaRegCopy className="text-slate-100 text-md" />
+						</div>
+					</div>
+				</div>
+				<div className="flex flex-col gap-2">
+					<span className="leading-none text-xs font-hubot font-semibold text-slate-100">
+						Options
+					</span>
+					<div className="px-3 py-2 bg-slate-800 rounded-md">
+						<span className="leading-none font-hubot text-xs text-slate-400">
+							Length
+						</span>
+						<input
+							type="number"
+							defaultValue={length}
+							value={length}
+							min="5"
+							max="128"
+							onBlur={(e) => handleLengthChange(e, true)}
+							onChange={handleLengthChange}
+							className="my-1 font-hubot text-white text-sm p-2 h-10 w-full rounded-md outline-1 outline-slate-400 focus:outline-2 focus:outline-blue-400"
+						/>
+						<span className="leading-none font-hubot text-xs text-slate-400">
+							Value must be between 5 and 128. Use 14 characters
+							or more to generate a strong password.
+						</span>
+					</div>
+				</div>
+				<div className="px-3 py-2 bg-slate-800 rounded-md">
+					<span className="leading-none font-hubot text-sm font-medium text-slate-100">
+						Include
+					</span>
+					<div className="py-2 flex justify-between items-center gap-1.5 font-hubot text-slate-100 text-xs antialiased font-medium">
+						{[
+							{
+								id: 'uppercase',
+								label: 'A-Z',
+								checked: uppercase,
+								onChange: () => setUppercase((prev) => !prev),
+							},
+							{
+								id: 'lowercase',
+								label: 'a-z',
+								checked: lowercase,
+								onChange: () => setLowercase((prev) => !prev),
+							},
+							{
+								id: 'numbers',
+								label: '0-9',
+								checked: numbers,
+								onChange: () => setNumbers((prev) => !prev),
+							},
+							{
+								id: 'symbols',
+								label: '!@#$%^&*',
+								checked: symbols,
+								onChange: () => setSymbols((prev) => !prev),
+							},
+						].map(({ id, label, checked, onChange }) => {
+							return (
+								<div
+									key={id}
+									className="flex items-center gap-1.5"
+								>
+									<input
+										type="checkbox"
+										id={id}
+										checked={checked}
+										className="scale-125"
+										onChange={onChange}
+									/>
+									<label
+										htmlFor={id}
+										className="select-none font-hubot text-slate-100 text-xs antialiased"
+									>
+										{label}
+									</label>
+								</div>
+							);
+						})}
+					</div>
+				</div>
+			</div>
+		</Modal>,
+		document.body
+	);
+};
+
+export default PasswordGenerator;
