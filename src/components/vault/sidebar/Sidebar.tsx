d3db8ddb9805e8a1194b3d1d2855e8f8bfef8083
James Michael <jomichael1994@gmail.com>
2025-06-22 01:18:02 +0100

    temporarily remove folders

--- src/components/vault/sidebar/Sidebar.tsx
+++ src/components/vault/sidebar/Sidebar.tsx
@@ -1,13 +1,13 @@
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 
 import { categories } from '../../../constants/sidebar';
 
 import SidebarSection from './SidebarSection';
 
-import type { RootState } from '../../../redux/store';
+// import type { RootState } from '../../../redux/store';
 
 const Sidebar = () => {
-	const folders = useSelector((state: RootState) => state.filter.folders);
+	// const folders = useSelector((state: RootState) => state.filter.folders);
 	return (
 		<div className="py-4 pl-4 pr-10 bg-slate-700 border-r-[1px] border-slate-950 flex flex-col gap-7 overflow-y-scroll">
 			<SidebarSection group="default" data={categories.default} />
@@ -16,12 +16,12 @@ const Sidebar = () => {
 				data={categories.types}
 				heading="Types"
 			/>
-			<SidebarSection
+			{/* <SidebarSection
 				group="folder"
 				data={folders}
 				heading="Folders"
 				onCreate={() => {}}
-			/>
+			/> */}
 		</div>
 	);
 };
