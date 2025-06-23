5aa71969c20ad132a001d1855b3fdcd37d3c71e4
James Michael <jomichael1994@gmail.com>
2025-06-23 23:20:32 +0100

    integrate user selection and fetch data

--- src/pages/auth/SelectUser.tsx
+++ src/pages/auth/SelectUser.tsx
@@ -1,25 +1,43 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import Users from '../../components/auth/Users';
 import LogInForm from '../../components/auth/LogInForm';
+import Loader from '../../components/shared/Loader';
 
 import type { ActiveUser } from '../../types/user';
 
-const users = [
-	{ id: '5b47e903-0ff9-459b-bca7-5297e2995e5f', username: 'James' },
-	// { id: 2, name: 'Test User' },
-];
+const VAULT_API_HOST = import.meta.env.VITE_VAULT_API_HOST!;
 
 const SelectUser = () => {
 	const [activeUser, setActiveUser] = useState<ActiveUser>(null);
 	const [showLogInForm, setShowLogInForm] = useState(false);
+	const [users, setUsers] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchUsers = async () => {
+			try {
+				const response = await axios.get(`${VAULT_API_HOST}/api/users`);
+				const data = response.data;
+				setUsers(data);
+			} catch (error) {
+				console.error('Failed to fetch users:', error.message);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+		fetchUsers();
+	}, []);
 
 	const handleUserSelection = (id: string, username: string) => {
 		setActiveUser({ id, username });
 		setShowLogInForm(true);
 	};
 
-	return showLogInForm ? (
+	return isLoading ? (
+		<Loader />
+	) : showLogInForm ? (
 		<LogInForm user={activeUser} onLogOut={() => setShowLogInForm(false)} />
 	) : (
 		<Users users={users} onSelect={handleUserSelection} />
