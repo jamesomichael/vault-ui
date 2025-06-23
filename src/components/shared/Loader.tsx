47c09dca97ff2c4c37e7aae0b9e4865e349a5027
James Michael <jomichael1994@gmail.com>
2025-06-23 23:03:41 +0100

    add loader component

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,11 @@
+const Loader = () => {
+	return (
+		<div className="flex items-center justify-center w-full h-full">
+			<div
+				className={`w-10 aspect-square border-2 border-t-transparent border-blue-400 rounded-full animate-spin`}
+			></div>
+		</div>
+	);
+};
+
+export default Loader;
