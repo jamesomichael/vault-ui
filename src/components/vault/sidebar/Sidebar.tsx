3daed8c147f32462b38c69b2ab7c07b8ef8a4537
James Michael <jomichael1994@gmail.com>
2025-08-07 10:31:42 +0100

    update demo watermark

--- src/components/vault/sidebar/Sidebar.tsx
+++ src/components/vault/sidebar/Sidebar.tsx
@@ -24,8 +24,8 @@ const Sidebar = () => {
 				onCreate={() => {}}
 			/> */}
 			</div>
-			<div className="px-3 py-4 flex flex-col gap-3">
-				<div className="border-1 border-slate-500 rounded p-6 flex justify-center items-center">
+			<div className="m-2 rounded px-3 py-4 flex flex-col gap-3 border-1 border-slate-500">
+				<div className="p-2 flex justify-center items-center">
 					<span className="font-hubot text-slate-500 text-xl font-light">
 						UI Demo
 					</span>
@@ -34,7 +34,7 @@ const Sidebar = () => {
 					href="https://github.com/jamesomichael/vault"
 					target="_blank"
 					rel="noopener noreferrer"
-					className="flex justify-center items-center hover:from-yellow-300 hover:to-yellow-400 bg-gradient-to-br from-yellow-400 to-yellow-500 rounded font-hubot px-3 py-2 font-bold text-sm"
+					className="transition-colors duration-300 flex justify-center items-center hover:from-yellow-500 hover:to-yellow-600 bg-gradient-to-br from-yellow-400 to-yellow-500 rounded font-hubot px-3 py-2 font-bold text-sm"
 				>
 					Get full version
 				</a>
