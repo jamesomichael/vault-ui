e475368e2d3cb62991b3917206b366defbec7d1e
James Michael <jomichael1994@gmail.com>
2025-08-14 00:54:18 +0100

    update action stories

--- src/components/shared/Action.stories.tsx
+++ src/components/shared/Action.stories.tsx
@@ -3,6 +3,7 @@ import { fn } from 'storybook/test';
 import Action from './Action';
 
 import { FaRegFloppyDisk, FaXmark, FaRegTrashCan } from 'react-icons/fa6';
+import { TbRestore } from 'react-icons/tb';
 
 import type { Meta, StoryObj } from '@storybook/react';
 
@@ -25,6 +26,26 @@ const meta: Meta<typeof Action> = {
 export default meta;
 
 export const Default: Story = {};
+
+export const Save: Story = {};
+
+export const Close: Story = {
+	args: {
+		title: 'Close',
+		onClick: fn(),
+		className: 'text-2xl text-slate-400 py-2.5',
+		Icon: FaXmark,
+	},
+};
+
+export const Restore: Story = {
+	args: {
+		title: 'Restore',
+		onClick: fn(),
+		className: 'text-2xl py-2.5',
+		Icon: TbRestore,
+	},
+};
 
 export const Destructive: Story = {
 	args: {
